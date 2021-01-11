2a8e72f3a0b566f4e14b9f59048a74c2036c950d
Luigi Ifan <61274357+luigiifan@users.noreply.github.com>
2021-01-11 14:28:15 +0700

    Update text-rpg.py

--- text-rpg.py
+++ text-rpg.py
@@ -1,47 +1,65 @@
+# Import
+import cmd
+import time
+import os
+import sys
+import textwrap
 import random
-from os import system
 
-player_health = 100
-# Monster Database
-commonEnemy_Type1 = [
-    {
-        'name': 'Ghoul',
-        'level': 3,
-        'hp': 82,
-        'counter': 'Fast',
-        'damage': random.randint(8,28),
-        'is_Boss': False,
-    },
-]
+screen_width = 100
 
-player_data = {
-        'name': 'Guest',
-        'skill': None,
-        'level': 1,
-        'hp': 100,
-        'weapon': 'Sword',
-        'armor': None,
-        'monster_kill': 0,
-        'inventory': None,
-        'is_Mastery': False,
-        'is_Hardcore': False
-}
+class player():
+    def __init__(self):
+        self.name = ''
+        self.hp = 0
+        self.mp = 0
+        self.status_effect = []
+        self.inventory = []
 
-while True:
-    get_Pname = str(input('Who are you? '))
-    if get_Pname.isdigit():
-        print('''Can't use number as a name! Try again.''')
-        continue
-    else:
-        break
+myPlayer = player()
 
-get_Pname = get_Pname.capitalize()
-player_data.update({'name': get_Pname})
-welcome_text = f'''Welcome to the game, {player_data['name']}!'''
-print(welcome_text)
-print('halo')
+# -------TITLE SCREEN---------
+def title_screen_choose():
+    Pcmd = input('>> ')
+    if Pcmd.lower() == ('main'):
+        start_game()
+    elif Pcmd.lower() == ('tutorial'):
+        help_game()
+    elif Pcmd.lower() == ('keluar'):
+        sys.exit()
+    while Pcmd.lower() not in ['main','tutorial','keluar']:
+        Print('Gunakan perintah yang benar! Coba lagi.')
+        Pcmd = input('>> ')
+        if Pcmd.lower() == ('main'):
+            start_game()
+        elif Pcmd.lower() == ('tutorial'):
+            help_game()
+        elif Pcmd.lower() == ('keluar'):
+            sys.exit()
 
-# ghoul_damage = commonEnemy_Type1[0]['damage']
-# print(f'Ghoul memberikan damage: -{ghoul_damage}')
-# playerhealth_now = player_health - ghoul_damage
-# print(f'Hp-mu sekarang: {playerhealth_now}')
+
+
+
+
+
+
+# while True:
+#     get_Pname = str(input('Who are you? '))
+#     if get_Pname.isdigit():
+#         print('''Can't use number as a name! Try again.''')
+#         continue
+#     else:
+#         break
+
+# get_Pname = get_Pname.capitalize()
+# player_data.update({'name': get_Pname})
+# os.system('cls')
+# print(f'''Welcome to game, {player_data['name']}!
+
+# >> This game is set in a village called OldKeep. You live in peace and quiet. Until that night,
+# Dreadsword's body was found in the middle of a farmer's field.Dreadsword is a cunning and-
+# ferocious monster. Dreadswords are usually found along beaches and in the middle of forests.
+# However, it seems that the explorers from the village informed that the monster came from a-
+# dark and scary Dungeon. You are assigned by the OldKeeper Leader to eradicate the monsters in-
+# The Dungoen so that they no longer disturb the peace of the villagers of OldKeep.
+# ''')
